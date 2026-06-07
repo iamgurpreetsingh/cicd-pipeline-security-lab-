05b28a8102f9bb9e38b37c754d9d6bbfc7ce35ed
iamgurpreetsingh <142823617+iamgurpreetsingh@users.noreply.github.com>
2026-06-07 14:56:31 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ def handler():
     secret = os.getenv("SECRET_KEY", "not_set")
     print("App running")
     print("Secret:", secret)
+    API_KEY = "sk_live_123456789"
 
 if __name__ == "__main__":
     handler()
